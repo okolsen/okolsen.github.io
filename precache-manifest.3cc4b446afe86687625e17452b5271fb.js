07902701266ac5bc74fd1e17e295652eedc61edb
okolsen <okolsen123@gmail.com>
2019-04-19 23:01:52 +0200

    First commit

---
+++ precache-manifest.3cc4b446afe86687625e17452b5271fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9ca2cd0578737f2fd872",
+    "url": "/static/js/main.9ca2cd05.chunk.js"
+  },
+  {
+    "revision": "ed6785a0870603772723",
+    "url": "/static/js/2.ed6785a0.chunk.js"
+  },
+  {
+    "revision": "9ca2cd0578737f2fd872",
+    "url": "/static/css/main.3a0ef7cb.chunk.css"
+  },
+  {
+    "revision": "ed6785a0870603772723",
+    "url": "/static/css/2.b6804945.chunk.css"
+  },
+  {
+    "revision": "52f0ed5138b9ec285a1f2152892b0615",
+    "url": "/index.html"
+  }
+];
